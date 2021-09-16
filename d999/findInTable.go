e406cb4339063fc9438937111f44b386580b872d
James Hughes <jim.d.hughes@gmail.com>
2021-09-16 12:54:56 -0600

    ...

--- d999/findInTable.go
+++ d999/findInTable.go
@@ -7,7 +7,7 @@ func FindInMatrix(matrixSize, value int) int {
 
 	for i <= j {
 		if i*j == value {
-			if (i == matrixSize && j == matrixSize) || (i == 1 && j == 1) {
+			if i == j {
 				counts += 1
 			} else {
 				counts += 2
