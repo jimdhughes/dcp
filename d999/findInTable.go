e954701751afc0da49b7eececd7a870b3d0101b6
James Hughes <jim.d.hughes@gmail.com>
2022-09-20 21:28:46 -0600

    changed something

--- d999/findInTable.go
+++ d999/findInTable.go
@@ -8,8 +8,10 @@ func CountOccurancesInMatrix(matrixSize, value int) int {
 	for i <= j {
 		if i*j == value {
 			if i == j {
+				// this is the diagonal
 				counts += 1
 			} else {
+				// this is for above / below diagnoal
 				counts += 2
 			}
 			i++
@@ -23,5 +25,18 @@ func CountOccurancesInMatrix(matrixSize, value int) int {
 		}
 	}
 
+	return counts
+}
+
+// create a function that finds the number of times a number appears in a multiplication table
+func GetNumberOfOccurancesInMultiplicationTable(matrixSize, value int) int {
+	var counts int
+	for i := 1; i <= matrixSize; i++ {
+		for j := 1; j <= matrixSize; j++ {
+			if i*j == value {
+				counts++
+			}
+		}
+	}
 	return counts
 }
